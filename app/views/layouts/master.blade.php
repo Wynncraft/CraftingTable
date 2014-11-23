7bc78b315d91c4a7bc34bac4bdd48c0bb5204f93
Jodie Belgrave <rmb1993@gmail.com>
2014-11-22 21:47:58 -0500

    added footer

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -13,6 +13,12 @@
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
         </div>
+
+        <div class="footer">
+            <div class="container">
+            <p class="text-muted">&copy; Jodie Belgrave 2014 <a href="https://github.com/Minestack" target="_blank">Github</a></p>
+            </div>
+        </div>
     </body>
 
 </html>
